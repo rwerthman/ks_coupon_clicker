c2006bd72058cbe39edb3446e41bd11c48f6749e
Robert Werthman <robertwerthman@Roberts-MBP.hsd1.mi.comcast.net>
2020-01-18 09:01:41 -0500

    Coupon clicking script is working again. Clicks a coupon once every second.  Probably could be faster... Kroger limits you to 150 coupons. Tested on https://www.kroger.com/cl/coupons/ using Chrome.
    fixes #1

--- contentScript.js
+++ contentScript.js
@@ -1,11 +1,31 @@
-function clickCoupons() {
+
+function timer( ms )
+{
+    return new Promise( resolve =>
+        setTimeout( resolve, ms ) );
+}
+
+async function clickCoupons() {
+
     let coupons = document.getElementsByClassName( "CouponActionButton" );
+
     console.log( "Number of coupons found", coupons.length );
 
     var i;
-    for( i = 0; i < 10; i++ )
+    var numCoupons = coupons.length;
+
+    console.log( "Clicking on coupons...");
+    for( i = 0; i < numCoupons; i++ )
     {
-        coupons[i].click();
+        var coupon = coupons[i];
+
+        if( "Clip" === coupon.textContent.trim() )
+        {
+            coupon.click();
+
+            // Pause before the next click
+            await timer( 1000 );
+        }
     }
 
     console.log( "Done clicking on coupons!" );
